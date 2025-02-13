43d15b71f1cba3b0f5fe57fc37ce5fcb2ca21612
SOLV-Code <gpestal@solv.ca>
2025-02-12 22:44:02 -0800

    calcHCR fn into server.R done

--- App/server.R
+++ App/server.R
@@ -80,138 +80,57 @@ library("gridExtra")
 
 
 	  if(input$display.tab == "Step"){
-
-
-	    step0.idx <- run.vec < input$step1.rp
-	    step1.idx <- run.vec >= input$step1.rp & run.vec < input$step2.rp
-      step2.idx <- run.vec >= input$step2.rp & run.vec < input$step3.rp
-      step3.idx <- run.vec >= input$step3.rp
-
-      hcr.out <- data.frame(Run = run.vec ,ER = NA)
-      hcr.out$ER[step0.idx] <- 0
-      hcr.out$ER[step1.idx] <- input$step1.rate
-      hcr.out$ER[step2.idx] <- input$step2.rate
-      hcr.out$ER[step3.idx] <- input$step3.rate
-
-      hcr.out$Spn <- hcr.out$Run * (1-hcr.out$ER/100)
-      hcr.out$Ct <- hcr.out$Run * hcr.out$ER/100
-
-	    #print(hcr.out)
-
-	  }
+        hcr.out <- calcHCR(run.vec = run.vec, hcr.type = input$display.tab ,
+	                       hcr.settings = list(step1.rp=input$step1.rp,
+	                                           step2.rp=input$step2.rp,
+	                                           step3.rp=input$step3.rp,
+	                                           step1.rate=input$step1.rate,
+	                                           step2.rate=input$step2.rate,
+	                                           step3.rate=input$step3.rate
+	                                           ))
+  	  }
 
 
 	  if(input$display.tab == "SlopedER"){
 
-	    # 4 ref points, each have run size and corresponding ER
-	    # this results in 5 zones:
-	    # Zone 0: Run is 0 to RP1 -> ER = 0
-	    # Zone 1: RP1 < Run <= RP2  -> slope and intercept based on points 1 and 2
-	    # Zone 2: RP2 < Run <= RP3  -> slope and intercept based on points 2 and 3
-	    # Zone 3: RP3 < Run <= RP4  -> slope and intercept based on points 3 and 4
-	    # Zone 4: Run larger than RP4 -> ER = ER4
-
-	    zone0.idx <- run.vec < input$slopeder.rp1
-	    zone1.idx <- run.vec >= input$slopeder.rp1 & run.vec < input$slopeder.rp2
-	    zone2.idx <- run.vec >= input$slopeder.rp2 & run.vec < input$slopeder.rp3
-	    zone3.idx <- run.vec >= input$slopeder.rp3 & run.vec < input$slopeder.rp4
-	    zone4.idx <- run.vec >= input$slopeder.rp4
-
-	    # line fit for Zone 1
-	    line1.pts <- data.frame(Run = c(input$slopeder.rp1,input$slopeder.rp2),
-	                            ER = c(input$slopeder.rate1,input$slopeder.rate2))
-	    line1.fit <- lm(ER ~ Run, data = line1.pts)
-      # lines 2,3
-	    line2.pts <- data.frame(Run = c(input$slopeder.rp2,input$slopeder.rp3),
-	                            ER = c(input$slopeder.rate2,input$slopeder.rate3))
-	    line2.fit <- lm(ER ~ Run, data = line2.pts)
-	    line3.pts <- data.frame(Run = c(input$slopeder.rp3,input$slopeder.rp4),
-	                            ER = c(input$slopeder.rate3,input$slopeder.rate4))
-	    line3.fit <- lm(ER ~ Run, data = line3.pts)
-
-
-
-	    hcr.out <- data.frame(Run = run.vec ,ER = NA)
-	    hcr.out$ER[zone0.idx] <- 0
-	    hcr.out$ER[zone1.idx] <- predict(line1.fit,newdata=hcr.out[zone1.idx,])
-	    hcr.out$ER[zone2.idx] <- predict(line2.fit,newdata=hcr.out[zone2.idx,])
-	    hcr.out$ER[zone3.idx] <- predict(line3.fit,newdata=hcr.out[zone3.idx,])
-	    hcr.out$ER[zone4.idx] <- input$slopeder.rate4
-
-	    hcr.out$Spn <- hcr.out$Run * (1-hcr.out$ER/100)
-	    hcr.out$Ct <- hcr.out$Run * hcr.out$ER/100
-
-	    #print(hcr.out)
+	    hcr.out <- calcHCR(run.vec = run.vec, hcr.type = input$display.tab ,
+	                       hcr.settings = list(slopeder.rp1=input$slopeder.rp1,
+	                                           slopeder.rp2=input$slopeder.rp2,
+	                                           slopeder.rp3=input$slopeder.rp3,
+	                                           slopeder.rp4 = input$slopeder.rp4,
+	                                           slopeder.rate1=input$slopeder.rate1,
+	                                           slopeder.rate2=input$slopeder.rate2,
+	                                           slopeder.rate3=input$slopeder.rate3,
+	                                           slopeder.rate4 =input$slopeder.rate4
+	                       ))
 
 	  }
 
 
-
-
-
-
-
 	  if(input$display.tab == "IceHockeyStick"){
 
+	    hcr.out <- calcHCR(run.vec = run.vec, hcr.type = input$display.tab ,
+	                       hcr.settings = list(ice.rp1=input$ice.rp1,
+	                                           ice.rp2=input$ice.rp2,
+	                                           ice.rate=input$ice.rate))
 
-	    low.idx <- run.vec <= input$ice.rp1
-	    high.idx <- run.vec >= input$ice.rp2
-	    mid.idx <- !low.idx & ! high.idx
-
-	    ice.slope <-  input$ice.rate / (input$ice.rp2 - input$ice.rp1)
-
-	    hcr.out <- data.frame(Run = run.vec ,ER = NA)
-
-	    hcr.out$ER[low.idx] <- 0
-	    hcr.out$ER[high.idx] <- input$ice.rate
-	    hcr.out$ER[mid.idx] <- (run.vec[mid.idx] -  input$ice.rp1) * ice.slope
-
-	    hcr.out$Spn <- hcr.out$Run * (1-hcr.out$ER/100)
-	    hcr.out$Ct <- hcr.out$Run * hcr.out$ER/100
-
-	    #print(hcr.out)
 
 	  }
 
 	  if(input$display.tab == "FieldHockeyStick"){
 
-	    hcr.out <- data.frame(Run = run.vec ,ER = NA)
-
-	    # calc abd above spn target
-	    abd.above.spn <- 	 hcr.out$Run  - input$field.rp1
-	    abd.above.spn[abd.above.spn<0] <- 0
-
-	    # calc potential rate (bounded by cap)
-	    hcr.out$ER <- round(abd.above.spn/hcr.out$Run,5)*100
-	    hcr.out$ER <- pmin(hcr.out$ER,input$field.rate)
-
-
-	    hcr.out$Spn  <- hcr.out$Run  * (1-hcr.out$ER/100)
-	    hcr.out$Ct <- hcr.out$Run  * (hcr.out$ER/100)
-
-
-	    #print(hcr.out)
-
+	    hcr.out <- calcHCR(run.vec = run.vec, hcr.type = input$display.tab ,
+	                       hcr.settings = list(field.rp1=input$field.rp1,
+	                                           field.rate=input$field.rate))
 	  }
 
 	  if(input$display.tab == "FieldHockeyStickwFloor"){
 
-      hcr.out <- data.frame(Run = run.vec ,ER = NA)
 
-      # calc abd above spn target
-      abd.above.spn <- 	 hcr.out$Run  - input$fieldwfloor.rp1
-      abd.above.spn[abd.above.spn<0] <- 0
-
-      # calc potential rate (bounded by cap)
-      hcr.out$ER <- round(abd.above.spn/hcr.out$Run,5)*100
-      hcr.out$ER <- pmin(hcr.out$ER,input$fieldwfloor.rate)
-      hcr.out$ER <- pmax(hcr.out$ER,input$fieldwfloor.floor)
-
-	    hcr.out$Spn  <- hcr.out$Run  * (1-hcr.out$ER/100)
-	    hcr.out$Ct <- hcr.out$Run  * (hcr.out$ER/100)
-
-
-	    #print(hcr.out)
+	    hcr.out <- calcHCR(run.vec = run.vec, hcr.type = input$display.tab ,
+	                       hcr.settings = list(fieldwfloor.rp1=input$fieldwfloor.rp1,
+	                                           fieldwfloor.rate=input$fieldwfloor.rate,
+	                                           fieldwfloor.floor = input$fieldwfloor.floor ))
 
 	  }
 
